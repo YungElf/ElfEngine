18d09320cf490e6f5d532b3213d402ec26e73a91
Alex R <alexgro.public@gmail.com>
2023-03-01 00:22:46 -0700

    Stage 2: still the tile class. added as much immutability as I could for the purposes of learning. it makes sense as well since chess is a very strict game. imported google's Guava libary to use ImmutableMap. Also got 99 fishing in OSRS earlier today! :)

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,12 +1,35 @@
 package com.chess.engine.board;
 
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import javax.management.ImmutableDescriptor;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Tile {
 
-     int tileCoordinate;
+     protected final int tileCoordinate;
 
-    Tile(int tileCoordinate){
+     private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+
+         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+         for(int i =0; i <64; i++){
+             emptyTileMap.put(i, new EmptyTile(i));
+         }
+
+         return ImmutableMap.copyOf(emptyTileMap);
+     }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+
+         return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
+    private Tile(int tileCoordinate){
         this.tileCoordinate = tileCoordinate;
     }
 
@@ -16,7 +39,7 @@ public abstract class Tile {
 
 
     public static final class EmptyTile extends Tile{
-        EmptyTile(int coordinate){
+        EmptyTile(final int coordinate){
             super(coordinate);
         }
         @Override
@@ -31,7 +54,7 @@ public abstract class Tile {
     }
 
     public static final class OccupiedTile extends Tile{
-        Piece pieceOnTile;
+        private final Piece pieceOnTile;
 
         OccupiedTile(int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
