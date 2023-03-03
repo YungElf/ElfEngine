b5534c9244666cebe610409168ee9cca2e6175e0
Alex R <alexgro.public@gmail.com>
2023-03-02 18:31:30 -0700

    Stage 3: Changes to Pieces, started working on the Knight. Added Alliance (color) as an enum since they're just a constant. Added getters for Alliance, pieces at certain tiles, and the tiles themselves

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -3,7 +3,6 @@ package com.chess.engine.board;
 import com.chess.engine.pieces.Piece;
 import com.google.common.collect.ImmutableMap;
 
-import javax.management.ImmutableDescriptor;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +10,7 @@ public abstract class Tile {
 
      protected final int tileCoordinate;
 
-     private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+     private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
      private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
 
@@ -20,13 +19,13 @@ public abstract class Tile {
          for(int i =0; i <64; i++){
              emptyTileMap.put(i, new EmptyTile(i));
          }
-
+         //Collections.unmodifiableMap(emptyTileMap)
          return ImmutableMap.copyOf(emptyTileMap);
      }
 
     public static Tile createTile(final int tileCoordinate, final Piece piece){
 
-         return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+         return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
 
     private Tile(int tileCoordinate){
@@ -39,7 +38,7 @@ public abstract class Tile {
 
 
     public static final class EmptyTile extends Tile{
-        EmptyTile(final int coordinate){
+        private EmptyTile(final int coordinate){
             super(coordinate);
         }
         @Override
@@ -56,7 +55,7 @@ public abstract class Tile {
     public static final class OccupiedTile extends Tile{
         private final Piece pieceOnTile;
 
-        OccupiedTile(int tileCoordinate, Piece pieceOnTile){
+        private OccupiedTile(int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
         }
