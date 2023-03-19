3c23702db5996085e47e40a26720dbe3bfa0ce77
Alex R <alexgro.public@gmail.com>
2023-03-19 00:06:38 -0700

    Stage 9: More and MORE Pawn work, added more exclusions, cleaned up some very long code. Still think it looks mildly like garbage but I don't think I can refactor to make it look better. I think I will leave the pawn alone for now and work on the King, which will finish up all the pieces (at least skeletal versions of them anyways)

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -4,6 +4,7 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,7 +14,7 @@ import static com.chess.engine.board.Move.*;
 
 public class Pawn extends Piece {
 
-    private final static int[] CANDIDATE_MOVE_COORDINATE = {8, 16};
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {7, 8, 9, 16};
 
     Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -26,7 +27,7 @@ public class Pawn extends Piece {
 
         for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
             //apply offset to piece position, get direction for white/black to set the way the pawn are moving
-            final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+            final int candidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * currentCandidateOffset);
 
             if (!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
                 continue;
@@ -44,9 +45,32 @@ public class Pawn extends Piece {
                         !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
                     legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
                 }
+                //if you are moving in the 7 direction and don't meet these exceptions....
+            } else if (currentCandidateOffset == 7 &&
+                    !((BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite() ||
+                            (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack())))) {
+                if (board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if (this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        //TODO handle attacking into a pawn promotion
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+
+
+            } else if (currentCandidateOffset == 9 &&
+                    !((BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite() ||
+                            (BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack())))) {
+                if (board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if (this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        //TODO handle attacking into a pawn promotion
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
             }
         }
 
-        return null;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
