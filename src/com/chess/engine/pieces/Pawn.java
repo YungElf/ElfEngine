a9e6dc8ba2c5fc2195d0baf2815b17ab2fae8c9e
Alex R <alexgro.public@gmail.com>
2023-03-18 23:41:53 -0700

    Stage 8: More Pawn work, dealt with the weird 2 tile move that pawns can do as a first move, so added a way to check if it was a piece's first move (WIP). Added a check to see if a piece was white or black that I can actually call. Added row exclusions to BoardUtils(WIP). Adding a lot of stubbed out garbage but will fix it later, stubs make it easier to write it out

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -11,39 +11,40 @@ import java.util.List;
 
 import static com.chess.engine.board.Move.*;
 
-public class Pawn extends Piece{
+public class Pawn extends Piece {
 
-    private final static int[] CANDIDATE_MOVE_COORDINATE = {8};
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {8, 16};
 
-
-
-
-
-    Pawn(int piecePosition, Alliance pieceAlliance) {
+    Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
     @Override
-    public Collection<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(final Board board) {
 
         final List<Move> legalMoves = new ArrayList<>();
 
-        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE){
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
             //apply offset to piece position, get direction for white/black to set the way the pawn are moving
-            int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+            final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
 
-            if(!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+            if (!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
                 continue;
             }
-            if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+            if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
 
-                //a lot more work to do here
+                //TODO add pawnMove, deal with promotions, finish BoardUtils row
                 legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
 
+            } else if (currentCandidateOffset == 16 && this.isFirstMove() &&
+                    (BoardUtils.SECOND_ROW(this.piecePosition) && this.getPieceAlliance().isBlack()) ||
+                    (BoardUtils.SEVENTH_ROW(this.piecePosition) && this.getPieceAlliance().isWhite())) {
+                final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+                if (!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                        !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                }
             }
-
-
-
         }
 
         return null;
